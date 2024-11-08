bea4f837c0cce4a91f02bdb3bb9af41ce5136faa
Sherin <sherinpshaju@gmail.com>
2024-11-08 20:43:38 +0530

    change2

--- src/views/authentication/auth/AuthLogin.js
+++ src/views/authentication/auth/AuthLogin.js
@@ -31,7 +31,7 @@ const AuthLogin = ({ title, subtitle, subtext }) => (
           htmlFor="username"
           mb="5px"
         >
-          UserName
+          User
         </Typography>
         <CustomTextField id="username" variant="outlined" fullWidth />
       </Box>

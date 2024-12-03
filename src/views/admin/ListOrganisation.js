e211ff6f6dccd444784279df2e9c3035b96c20ad
Kaviya <kaviyavarshinisr@gmail.com>
2024-12-03 19:32:33 +0530

    last few changes

--- src/views/admin/ListOrganisation.js
+++ src/views/admin/ListOrganisation.js
@@ -1,51 +1,46 @@
 import React from "react";
-import { useNavigate } from "react-router-dom"; // Import useNavigate;
+import { useNavigate } from "react-router-dom";
 import { Box } from "@mui/material";
 import PageContainer from 'src/components/container/PageContainer';
-import { Grid, Button, Typography } from "@mui/material";
-
-import { useParams } from "react-router-dom"; // Import useParams
+import { Grid, Button } from "@mui/material";
+import { useParams } from "react-router-dom";
 import AdminHeader from "./AdminHeader";
 import BreadcrumbComponent from "../../components/shared/BreadCrumbComponent";
 import Organisationstable from "./organisation/Organisationstable";
 
 const ListOrganisation = () => {
-  const { organizationName } = useParams(); // Extract organisationName from URL params
+  const { organizationName } = useParams();
 
-  const navigate = useNavigate(); // Initialize navigate function
+  const navigate = useNavigate();
 
   const handleAddOrganisationClick = () => {
-    navigate('/admin/addorganisation'); // Navigate to the create user page
+    navigate('/admin/addorganisation');
   };
 
   return (
     <PageContainer
-      title="Users"
-      description="List of users for the selected organization"
+      title="Organisations"
+      description="List of organisations"
     >
       <AdminHeader />
-      <BreadcrumbComponent
-        pageTitle="Organisations Table"
-        breadcrumbTitle1="Organisation"
-        breadcrumbRoute1="/admin/organisation"
-        marginTop="70px"
-      />
+      <Box display="flex" justifyContent="space-between" alignItems="center" mt={5} mb={2}>
+        <BreadcrumbComponent
+          pageTitle="Organisations Table"
+          breadcrumbTitle1="Organisation"
+          breadcrumbRoute1="/admin/organisation"
+          marginTop="0"
+        />
+        <Button
+          variant="contained"
+          onClick={handleAddOrganisationClick}
+        >
+          Add Organisation +
+        </Button>
+      </Box>
       {/* Main content layout */}
-      <Box marginLeft={1} marginTop={1} marginRight={-5}>
+      <Box marginLeft={1} marginTop={1} marginRight={1}>
         <Grid container spacing={3}>
           <Grid item xs={12} lg={12}>
-            <Box position="relative" mb={2}>
-              {/* Add organisation button */}
-              <Box position="absolute" bottom={-10} left={90} mb={2}>
-                <Button
-                  variant="contained"
-                  onClick={handleAddOrganisationClick}
-                >
-                  Add Organisation +
-                </Button>
-              </Box>
-            </Box>
-            {/* organisation table */}
             <Organisationstable />
           </Grid>
         </Grid>

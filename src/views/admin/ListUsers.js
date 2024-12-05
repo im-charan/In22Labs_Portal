13b5f34924a75e6c3697dfae34117aec54cd1993
Sherin <sherinpshaju@gmail.com>
2024-12-05 18:26:14 +0530

    backend setup

--- src/views/admin/ListUsers.js
+++ src/views/admin/ListUsers.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useNavigate } from "react-router-dom"; // Import useNavigate;
 import { Box, Grid, Button, Typography } from "@mui/material";
 import PageContainer from 'src/components/container/PageContainer';
+import { Grid, Button } from "@mui/material";
 import UserTable from "./users/userTable";
 import { useParams } from "react-router-dom"; // Import useParams
 
@@ -9,7 +10,7 @@ import BreadcrumbComponent from "../../components/shared/BreadCrumbComponent";
 import AdminHeader from "./AdminHeader";
 
 const ListUsers = () => {
-  const { organizationName } = useParams(); // Extract organisationName from URL params
+  const { organizationName } = useParams(); // Extract organizationName from URL params
 
   const navigate = useNavigate(); // Initialize navigate function
 
@@ -20,32 +21,21 @@ const ListUsers = () => {
   return (
     <PageContainer title="Users" description="List of users for the selected organization">
       <AdminHeader />
-      
-      {/* Breadcrumb and Create User Button */}
-      <Box 
-        display="flex" 
-        justifyContent="space-between" 
-        alignItems="center" 
-        mb={3} // Adds spacing below
-      >
-        {/* Breadcrumb Component */}
+      <Box display="flex" justifyContent="space-between" alignItems="center" mt={5} mb={2}>
         <BreadcrumbComponent  
           pageTitle="User Table" 
           breadcrumbTitle1="User"
           breadcrumbRoute1="/admin/users"
-          marginTop="70px"
+          marginTop="0" // Adjust marginTop for alignment
         />
-        {/* Create User Button */}
         <Button variant="contained" onClick={handleCreateUserClick}>
           Create User
         </Button>
       </Box>
-
       {/* Main content layout */}
       <Box marginLeft={'3px'} marginTop={0}>
         <Grid container spacing={3}>
           <Grid item xs={12} lg={12}>
-            {/* User Table */}
             <UserTable />
           </Grid>
         </Grid>
